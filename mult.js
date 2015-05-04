c1b44e5c83145720c006cb0384af1e26fb886127
Craig <cav9871@saintjoe.edu>
2015-05-04 13:55:23 -0400

    exercise one from exam

---
+++ mult.js
@@ -0,0 +1,10 @@
+
+
+function mult(num1,num2) {				//takes 2 parameters
+	num1++;								// increments first and second number
+	num2++;
+	var ans = num1 * num2;				// multiplies the numbers and stores it in ans
+	print(ans);							// prints the answer out
+
+}
+mult(2,3);
